def8214fdec8e5e4ce5ffce645a2eecab4ce0719
DenysMoshnin <denys.moshnin@gmail.com>
2019-09-21 19:39:10 +0300

    + пример

---
+++ №020/AudioPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _020
+{
+    class AudioPlayer
+    {
+        private int volume;
+        public int Volume
+        {
+            set
+            {
+                if (value >= 0 && value <= 100) //ограничиваем принимаемое значение громкости в районе от 0 до 100
+                {
+                    volume = value;
+                }
+                else
+                {
+                    if (value < 0)
+                    {
+                        volume = 0;
+                    }
+                    else if (value > 100)
+                    {
+                        volume = 100;
+                    }
+
+                }
+            }
+            get
+            {
+                return volume;
+            }
+        }
+    }
+}
---
+++ №020/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Создайте класс Аудиоплеер, в котором есть поле громкость звука, для доступа к этому полю реализуйте свойство. Громкость может быть в диапазоне от 0 до 100
+             */
+
+            Console.WriteLine("Введите громкость: ");
+            AudioPlayer ap = new AudioPlayer();
+            ap.Volume = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nГромкость:" + ap.Volume);
+        }
+    }
+}
