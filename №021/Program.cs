11b560c211dd1437ff93307ae4e2b31bab97084e
DenysMoshnin <denys.moshnin@gmail.com>
2019-09-21 22:57:55 +0300

    + задача

---
+++ №021/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Напишите программу – эмулятор работы холодильника. Программа должна позволять выполнять через меню в консольном приложении те или иные действия с холодильником, 
+                например, включать или выключать его, и т.п. При этом после каждого действия пользователя, программа должна отобразить на экране текущее состояние холодильника и меню всех функций.
+                Класс холодильника должен иметь метод, отображающий на экран его текущее состояние, а также методы, позволяющие это состояние изменять.
+                Главный метод программы (Main) должен выполнять задачу отображения меню и передачу команд пользователя объекту класса холодильника.   
+           */
+            Refrigerator RF = new Refrigerator(true, 5, -10);
+            while (true)
+            {
+                Console.Clear();
+                RF.Info();
+                RF.ComandMenu();
+                Console.Write("\nВведите команду: ");
+                string[] commands = Console.ReadLine().Split(' ');
+                if (commands.Length == 1)
+                {
+                    if (commands[0] == "exit")
+                    {
+                        return;
+                    }
+                    if (commands[0] == "power")
+                    {
+                        RF.SwichStatus();
+                        continue;
+                    }
+                    else
+                    {
+                        RF.Eror();
+                        continue;
+                    }
+                }
+                if (commands.Length == 2)
+                {
+                    if (commands[0] == "plus")
+                    {
+                        if (commands[1] == "ref")
+                        {
+                            RF.PlusTempRef();
+                            continue;
+                        }
+                        if (commands[1] == "fr")
+                        {
+                            RF.PlusTempFr();
+                            continue;
+                        }
+                    }
+                    if (commands[0] == "dec")
+                    {
+                        if (commands[1] == "ref")
+                        {
+                            RF.DecTempRef();
+                            continue;
+                        }
+                        if (commands[1] == "fr")
+                        {
+                            RF.DecTempRef();
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        RF.Eror();
+                        continue;
+                    }
+                }
+                else
+                {
+                    RF.Eror();
+                }
+            }
+        }
+    }
+}
---
+++ №021/Refrigerator.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _021
+{
+    class Refrigerator
+    {
+        private bool status;
+        private int tempRef;
+        private int tempFr;
+        public bool Status
+        {
+            set
+            {
+                value = status;
+            }
+            get
+            {
+                return status;
+            }
+        }
+        public int TempRef
+        {
+            set
+            {
+                if (value >= 5 && value <= 15) //ограничиваем принимаемую температуру в районе от 5 до 15
+                {
+                    tempRef = value;
+                }
+                else
+                {
+                    if (value < 5)
+                    {
+                        tempRef = 5;
+                    }
+                    else if (value > 15)
+                    {
+                        tempRef = 15;
+                    }
+
+                }
+            }
+            get
+            {
+                return tempRef;
+            }
+        }
+        public int TempFr
+        {
+            set
+            {
+                if (value <= -5 && value >= -24) //ограничиваем принимаемую температуру в районе от 5 до 15
+                {
+                    tempFr = value;
+                }
+                else
+                {
+                    if (value > -5)
+                    {
+                        tempFr = -5;
+                    }
+                    else if (value < -24)
+                    {
+                        tempFr = -24;
+                    }
+
+                }
+            }
+            get
+            {
+                return tempFr;
+            }
+        }
+        public Refrigerator(bool status, int tempRef, int tempFr)
+
+        {
+            Status = status;
+            TempFr = tempFr;
+            TempRef = tempRef;
+        } //создаем холодильник
+        public void SwichStatus()
+        {
+            if (status == true)
+            {
+                status = false;
+            }
+            else
+            {
+                status = true;
+            }
+        } //вкл или выкл 
+        public void PlusTempRef()
+        {
+            TempRef++;
+        } //увеличить темп холодильника
+        public void DecTempRef()
+        {
+            TempRef--;
+        } //увеличить темп холодильника
+        public void PlusTempFr()
+        {
+            TempFr++;
+        } //увеличить темп морозилки
+        public void DecTempFr()
+        {
+            TempFr--;
+        } //уменьшить темп морозилки
+        public void Info()
+        {
+            if (status == true) //включенный холодильник
+            {
+                Console.WriteLine("Холодильник: " + status + "\nТемпература холодильника: " + tempRef + "\nТемпература морозилки: " + tempFr);
+            }
+            else
+            {
+                Console.WriteLine("Холодильник: " + status); //выключенный
+            }
+
+        }
+        public void ComandMenu() //список команд
+        {
+            Console.WriteLine("\npower - включить или выключить холодильник;\nplus ref/fr - увеличить температуру холодильника/морозилки;" +
+                "\ndec ref/fr - уменьшить теипературу холодильника/морозилки\nexit - выйти из меню");
+        }
+        public void Eror() //неучтенный вод
+        {
+            Console.WriteLine("\n\tКоманда была введена неправильно, или такой команды не существует");
+            Console.WriteLine("\nexit - выход.");
+            Console.WriteLine("Нажмите любую клавишу для продолжения");
+            Console.ReadKey();
+        }
+    }
+}
